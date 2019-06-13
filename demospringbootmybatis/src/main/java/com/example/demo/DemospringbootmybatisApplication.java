737aa8fc78abbf9d845213b050152bded076949b
hubeiaijin <45006306+hubeiaijin@users.noreply.github.com>
2019-06-14 00:29:44 +0800

    Update DemospringbootmybatisApplication.java

--- demospringbootmybatis/src/main/java/com/example/demo/DemospringbootmybatisApplication.java
+++ demospringbootmybatis/src/main/java/com/example/demo/DemospringbootmybatisApplication.java
@@ -9,7 +9,8 @@ public class DemospringbootmybatisApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemospringbootmybatisApplication.class, args);
 	//测试git
-	
+
+	//远程写的内容 下拉到本地时注意.
 	}
 
 }

145a026da952171aaacee8f2881bb4c42c2d6138
“hubeiajin” <1248121764@qq.com>
2019-06-14 00:08:24 +0800

    第一次提交你就成功吧

---
+++ demospringbootmybatis/src/main/java/com/example/demo/DemospringbootmybatisApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringbootmybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringbootmybatisApplication.class, args);
+	}
+
+}
